15bba23b5334a7576bba494da5fe0735b31a3e30
Deepak Surti <dmsurti@gmail.com>
2017-06-11 16:09:06 +0530

    Add block mesh dict

---
+++ reynolds/blockmesh/mesh_dict.py
@@ -0,0 +1,58 @@
+class MeshDict(object):
+    def __init__(self, convert_to_meters, vertices, block, regions, edges=None, mergePairs=None):
+        self.convert_to_meters = convert_to_meters
+        self.vertices = vertices
+        self.block = block
+        self.regions = regions
+        self.edges = edges
+        self.mergePairs = mergePairs
+
+    def header_string(self):
+        header_comps = ['FoamFile',
+                        '{',
+                        '\tversion\t2.0;',
+                        '\tformat\tascii;',
+                        '\tclass\tdictionary;',
+                        '\tobject\tblockMeshDict;',
+                        '}']
+        return '\n'.join(s for s in header_comps)
+
+    def meters_string(self):
+        return 'convertToMeters {}'.format(self.convert_to_meters)
+
+    def vertices_string(self):
+        vertices_comps = ['vertices', '('] + ['\t' + v.dict_string() for v in self.vertices] + [');']
+        return '\n'.join(s for s in vertices_comps)
+
+    def blocks_string(self):
+        b_comps = ['blocks', '('] + ['\t' + self.block.dict_string()] + [');']
+        return '\n'.join(s for s in b_comps)
+
+    def boundary_string(self):
+        r_comps = ['\t' + r1 for r in self.regions for r1 in r.dict_string().split('\n')]
+        b_comps = ["boundary", '('] + r_comps + [');']
+        return '\n'.join(br for br in b_comps)
+
+    #TODO: Implement dict string for edges as a mesh component
+    def edges_string(self):
+        e_comps = ['edges', '('] + [');']
+        return '\n'.join(s for s in e_comps)
+
+    # TODO: Implement dict string for mergePatchPairs as a mesh component
+    def merge_pairs_string(self):
+        e_comps = ['mergePatchPairs', '('] + [');']
+        return '\n'.join(s for s in e_comps)
+
+    def dict_string(self):
+        d_comps = [self.header_string(),
+                   '\n',
+                   self.meters_string(),
+                   '\n',
+                   self.blocks_string(),
+                   '\n',
+                   self.edges_string(),
+                   '\n',
+                   self.boundary_string(),
+                   '\n',
+                   self.merge_pairs_string()] =
+        return '\n'.join(s for s in d_comps)
---
+++ reynolds/tests/test_mesh_dict.py
@@ -0,0 +1,92 @@
+from unittest import  TestCase
+from reynolds.blockmesh.mesh_dict import MeshDict
+from reynolds.blockmesh.mesh_components import Vertex3, Block, SimpleGrading, BoundaryRegion, Face
+import os
+
+
+class TestMeshDict(TestCase):
+    def setUp(self):
+        v0 = Vertex3(0, 0, 0)
+        v1 = Vertex3(1, 0, 0)
+        v2 = Vertex3(1, 1, 0)
+        v3 = Vertex3(0, 1, 0)
+        v4 = Vertex3(0, 0, 0.1)
+        v5 = Vertex3(1, 0, 0.1)
+        v6 = Vertex3(1, 1, 0.1)
+        v7 = Vertex3(0, 1, 0.1)
+        self.vertices = [v0, v1, v2, v3, v4, v5, v6, v7]
+
+        sg = SimpleGrading([1, 1, 1])
+        self.block = Block(range(8), [20, 20, 1], sg)
+
+        # movingWall region
+        self.mw1 = Face([3, 7, 6, 2])
+        self.mw_br = BoundaryRegion("movingWall", "wall", [self.mw1])
+        # fixedWalls region
+        self.fw1 = Face([0, 4, 7, 3])
+        self.fw2 = Face([2, 6, 5, 1])
+        self.fw3 = Face([1, 5, 4, 0])
+        self.fw_br = BoundaryRegion("fixedWalls", "wall", [self.fw1, self.fw2, self.fw3])
+        # frontAndBack region
+        self.fb1 = Face([0, 3, 2, 1])
+        self.fb2 = Face([4, 5, 6, 7])
+        self.fb_br = BoundaryRegion("frontAndBack", "empty", [self.fb1, self.fb2])
+
+        self.regions = [self.mw_br, self.fw_br, self.fb_br]
+
+        self.mesh_dict = MeshDict(0.1, self.vertices, self.block, self.regions)
+
+    def test_header_string(self):
+        header_comps = ['FoamFile',
+                        '{',
+                        '\tversion\t2.0;',
+                        '\tformat\tascii;',
+                        '\tclass\tdictionary;',
+                        '\tobject\tblockMeshDict;',
+                        '}']
+        header_string = '\n'.join(s for s in header_comps)
+        self.assertEqual(header_string, self.mesh_dict.header_string())
+
+    def test_vertices(self):
+        vertices_comps = ['vertices', '('] + ['\t' + v.dict_string() for v in self.vertices] + [');']
+        v_string = '\n'.join(s for s in vertices_comps)
+        self.assertEqual(v_string, self.mesh_dict.vertices_string())
+
+    def test_blocks(self):
+        b_comps = ['blocks', '('] + ['\t' + self.block.dict_string()] + [');']
+        b_string = '\n'.join(s for s in b_comps)
+        self.assertEqual(b_string, self.mesh_dict.blocks_string())
+
+    def test_boundary(self):
+        mw_face_comps = ['\t\t' + f.dict_string() for f in [self.mw1]]
+        mw_string_comps = ["movingWall", '{', '\ttype wall;', '\tfaces', '\t('] + mw_face_comps + ['\t);', '}']
+        mw_string = '\n'.join('\t' + s for s in mw_string_comps)
+
+        fw_face_comps = ['\t\t' + f.dict_string() for f in [self.fw1, self.fw2, self.fw3]]
+        fw_string_comps = ["fixedWalls", '{', '\ttype wall;', '\tfaces', '\t('] + fw_face_comps + ['\t);', '}']
+        fw_string = '\n'.join('\t' + s for s in fw_string_comps)
+
+        fb_face_comps = ['\t\t' + f.dict_string() for f in [self.fb1, self.fb2]]
+        fb_string_comps = ["frontAndBack", '{', '\ttype empty;', '\tfaces', '\t('] + fb_face_comps + ['\t);', '}']
+        fb_string = '\n'.join('\t' + s for s in fb_string_comps)
+
+        b_string_comps = ["boundary", '(', mw_string, fw_string, fb_string, ');']
+        b_string = '\n'.join(s for s in b_string_comps)
+        print('Expected output:\n\n' + b_string)
+        self.assertEqual(b_string, self.mesh_dict.boundary_string())
+
+    def test_edges(self):
+        e_string = 'edges\n' + '(\n' + ');'
+        self.assertEqual(e_string, self.mesh_dict.edges_string())
+
+    def test_merge_pairs(self):
+        m_string = 'mergePatchPairs\n' + '(\n' + ');'
+        self.assertEqual(m_string, self.mesh_dict.merge_pairs_string())
+
+    def test_dict_string(self):
+        print(self.mesh_dict.dict_string())
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        sample_dict_file_path = os.path.join(dir_path, 'blockMeshDictCavitySample')
+        with open(sample_dict_file_path, 'r') as f:
+            d_string = f.read()
+            self.assertEqual(d_string, self.mesh_dict.dict_string())
