ae04ce68e539e294e6a2aea6f7234e606a23bbb5
Deepak Surti <dmsurti@gmail.com>
2017-06-16 08:59:14 -0700

    Handle starting openfoam on linux and macos

--- reynolds/foam/start.py
+++ reynolds/foam/start.py
@@ -1,32 +1,48 @@
 from subprocess import Popen, PIPE
 import os
+from sys import platform
 from os import path
 
 class FoamRunner(object):
     def __init__(self):
-        self.foam_path = path.join(path.expanduser('~'), 'OpenFOAM')
-        self.source_path = path.join(self.foam_path, 'OpenFOAM-4.x', 'etc', 'bashrc')
+        if platform == "linux" or platform == "linux2":
+            self.foam_path = path.join("/", "opt", "openfoam4")
+            self.source_path = path.join(self.foam_path, 'etc', 'bashrc')
+        elif platform == "darwin":
+            self.foam_path = path.join(path.expanduser('~'), 'OpenFOAM')
+            self.source_path = path.join(self.foam_path, 'OpenFOAM-4.x', 'etc', 'bashrc')
+        else:
+            raise AssertionError("Unsupported platform: ", platform)
         print(self.source_path)
 
+
     def start(self):
-        foam_proc = Popen(['hdiutil', 'attach', '-mountpoint', self.foam_path,
-                           'OpenFOAM.sparsebundle'], cwd=path.expanduser('~'))
-        out, err = foam_proc.communicate()
-        status = foam_proc.poll()
-        if status == 0:
-            print("Source openfoam env vars...")
+        if platform == "linux" or platform == "linux2":
             self.shell_source(self.source_path)
             return True
+        elif platform == "darwin":
+            foam_proc = Popen(['hdiutil', 'attach', '-mountpoint', self.foam_path,
+                               'OpenFOAM.sparsebundle'], cwd=path.expanduser('~'))
+            out, err = foam_proc.communicate()
+            status = foam_proc.poll()
+            if status == 0:
+                print("Source openfoam env vars...")
+                self.shell_source(self.source_path)
+                return True
+            else:
+                print("Could not start OpenFOAM")
+                return False
         else:
-            print("Could not start OpenFOAM")
-            return False
+            raise AssertionError("Unsupported platform: ", platform)
+
 
     def shell_source(self, script):
         """Sometime you want to emulate the action of "source" in bash,
         settings some environment variables. Here is a way to do it."""
         pipe = Popen(". %s; env" % script, stdout=PIPE, shell=True)
         output = pipe.communicate()[0]
-        env = dict((line.decode().split('=') for line in output.splitlines() if '=' in line.decode()))
+        env = dict((line.decode().split('=', 1) for line in output.splitlines()
+                    if '=' in line.decode()))
         os.environ.update(env)
         print('tutorials: ', os.environ['FOAM_TUTORIALS'])
 
