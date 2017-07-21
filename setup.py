d3e1c180f2e27bbab4c0ac524ac2d25a4b4f2610
Deepak Surti <dmsurti@gmail.com>
2017-07-21 13:29:09 +0530

    Copy json, schema, foam template resources

--- setup.py
+++ setup.py
@@ -31,7 +31,10 @@ from distutils.core import setup
 setup(
         name='reynolds',
         version='0.0.1',
-        packages=['reynolds', 'reynolds.foam', 'reynolds.tests', 'reynolds.solver', 'reynolds.blockmesh'],
+        packages=['reynolds', 'reynolds.foam', 'reynolds.tests',
+                  'reynolds.solver', 'reynolds.blockmesh',
+                  'reynolds.json', 'reynolds.dict'],
+        package_data={'':['**/*.json', '**/*.schema', '**/*.foam']},
         url='https://github.com/dmsurti/reynolds',
         license='GPL v3.0',
         author='Deepak Surti',
